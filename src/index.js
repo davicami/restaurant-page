32e9d68003bce0ab0d0e692c8e9d6fb0bd94ee20
davicami <100150296+davicami@users.noreply.github.com>
2022-05-20 18:46:21 +0200

    Html file created by js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,48 @@
 console.log("Loadingg");
+
+function initializeWebsite() {
+    const content = document.getElementById("content");
+  
+    content.appendChild(createHeader());
+    //content.appendChild(createMain());
+    //ontent.appendChild(createFooter());
+  
+    //setActiveButton(document.querySelector(".button-nav"));
+    //loadHome();
+  }
+
+function createHeader() {
+    const header = document.createElement("div");
+    header.classList.add("header");
+  
+    const restaurantLogo = document.createElement("div");
+    restaurantLogo.textContent = "Akira Sushi";
+  
+    header.appendChild(restaurantLogo);
+    header.appendChild(createNav());
+  
+    return header;
+  }
+
+  function createNav() {
+    const nav = document.createElement("nav");
+    const navList = document.createElement("ul");
+    navList.classList.add("nav-list");
+
+    const homeLi = document.createElement("li");
+    homeLi.textContent = "Home";
+
+    const menuLi = document.createElement("li");
+    menuLi.textContent = "Menu";
+
+    const contactLi = document.createElement("li");
+    contactLi.textContent = "Contact";
+
+    navList.appendChild(homeLi);
+    navList.appendChild(menuLi);
+    navList.appendChild(contactLi);
+    nav.appendChild(navList);
+    return nav;
+  }
+
+  initializeWebsite();
